2bd7bcc7d45c27846b4ae10a56d7c03e3736ca80
HelpCplusplus <144713998+HelpCplusplus@users.noreply.github.com>
2023-10-19 10:02:33 +0800

    Delete web code directory

--- web code/jsss/main.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector('.menu-list');
-    const isVisible = getComputedStyle(menu).display === "block";
-
-    menu.style.display = isVisible ? "none" : "block";
-}
